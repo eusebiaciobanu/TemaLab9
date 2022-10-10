ba3cca144f3d87e7318088bc6e971e91acf9f412
Eusebia Ciobanu <eusebiaciobanu@MacBook-Air-Eusebia.local>
2022-10-10 17:21:13 +0300

    First commit

---
+++ src/Main.java
@@ -0,0 +1,38 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("--- Exercitiu #1 ---");
+        System.out.println("Hello");
+        System.out.println("Eusebia");
+
+        System.out.println("--- Exercitiu #2 ---");
+
+        int number1 = 10;
+        int number2 = 25;
+        int result1 = number1 + number2;
+
+        System.out.println("Rezultatul sumei dintre " + number1 + " si " + number2 + " este: " + result1);
+
+        System.out.println("--- Exercitiu #3 ---");
+
+        int number3 = 10;
+        int number4 = 3;
+        float result2  = (float) (number3) / (number4);
+        System.out.println("Rezultatul impartirii dintre " + number3 + " si " + number4 + " este: " + result2);
+
+        System.out.println("--- Exercitiu #4.a---");
+        int result3 = -5+8*6;
+        System.out.println("Rezultatul este: " + result3);
+
+        System.out.println("--- Exercitiu #4.b---");
+        float result4 = (55+9) % 9;
+        System.out.println("Rezultatul este: " + result4);
+
+        System.out.println("--- Exercitiu #4.c---");
+        float result5 = 20 + (float) -3*5 / 8;
+        System.out.println("Rezultatul este: " + result5);
+
+        System.out.println("--- Exercitiu #4.d---");
+        float result6 = 5 + (float) 15 / 3 * 2 - 8 % 3;
+        System.out.println("Rezultatul este: " + result6);
+    }
+}
